7e8f0bf39a2c2172232fbe4e4252895c41a8ad26
Shawn Garbett <Shawn.P.Garbett@Vanderbilt.edu>
2017-03-01 10:42:54 -0600

    Adding double documentation

--- R/double.R
+++ R/double.R
@@ -1,2 +1,16 @@
+#' A Doubling Function
+#'
+#' Sometimes you just need twice as much. This function can provide that for any 
+#' numerical vector passed in.
+#' 
+#' Additional paragraph to make sure we have twice as much verbage.
+#' 
+#' @param x what you need twice as much of
+#' @keywords simple
+#' @export
+#' @examples 
+#' double(5)
+#' double(3:10)
+#' double("X") # This is a mistake!
 
 double <- function(x) 2*x
